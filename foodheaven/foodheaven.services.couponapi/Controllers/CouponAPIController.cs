db7cf80ddac939ded608c5679659aa65a9ad80bc
Nanda, Ashish <Ashish.Nanda@ellucian.com>
2023-09-21 22:56:37 +0530

    Coupon Service Initial Check-in#

---
+++ foodheaven/foodheaven.services.couponapi/Controllers/CouponAPIController.cs
@@ -0,0 +1,129 @@
+﻿using AutoMapper;
+using Azure;
+using foodheaven.services.couponapi.Data;
+using foodheaven.services.couponapi.Models;
+using foodheaven.services.couponapi.Models.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace foodheaven.services.couponapi.Controllers
+{
+    [Route("api/coupon")]
+    [ApiController]
+    public class CouponAPIController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+        private ResponseDto _responseDto;
+        private readonly IMapper _mapper;
+
+        public CouponAPIController(ApplicationDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+            _responseDto = new ResponseDto();
+        }
+
+        [HttpGet]
+        public ResponseDto Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> coupons = _db.Coupons.ToList();
+                _responseDto.Result = _mapper.Map<IEnumerable<Coupon>>(coupons);
+            }
+            catch (Exception ex)
+            {
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = ex.Message;
+            }
+            return _responseDto;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public ResponseDto Get(int id) 
+        {
+            try
+            {
+                Coupon? coupon = _db.Coupons.First<Coupon>(x => x.CouponId == id);
+                _responseDto.Result = _mapper.Map<CouponDto>(coupon);
+            }
+            catch (Exception ex)
+            {
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = ex.Message;
+            }
+            return _responseDto;
+        }
+
+        [HttpGet]
+        [Route("GetByCode/{code}")]
+        public ResponseDto GetByCode(string code)
+        {
+            try
+            {
+                Coupon obj = _db.Coupons.First(u => u.CouponCode.ToLower() == code.ToLower());
+                _responseDto.Result = _mapper.Map<CouponDto>(obj);
+            }
+            catch (Exception ex)
+            {
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = ex.Message;
+            }
+            return _responseDto;
+        }
+
+        [HttpPost]
+        public ResponseDto Post([FromBody] CouponDto couponDto)
+        {
+            try
+            {
+                Coupon obj = _mapper.Map<Coupon>(couponDto);
+                _db.Coupons.Add(obj);
+                _db.SaveChanges();
+                _responseDto.Result = _mapper.Map<CouponDto>(obj);
+            }
+            catch (Exception ex)
+            {
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = ex.Message;
+            }
+            return _responseDto;
+        }
+
+        [HttpPut]
+        public ResponseDto Put([FromBody] CouponDto couponDto)
+        {
+            try
+            {
+                Coupon obj = _mapper.Map<Coupon>(couponDto);
+                _db.Coupons.Update(obj);
+                _db.SaveChanges();
+                _responseDto.Result = _mapper.Map<CouponDto>(obj);
+            }
+            catch (Exception ex)
+            {
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = ex.Message;
+            }
+            return _responseDto;
+        }
+
+        [HttpDelete]
+        public ResponseDto Delete(int id)
+        {
+            try
+            {
+                Coupon obj = _db.Coupons.First(x => x.CouponId == id);
+                _db.Coupons.Remove(obj);
+                _db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = ex.Message;
+            }
+            return _responseDto;
+        }
+    }
+}
---
+++ foodheaven/foodheaven.services.couponapi/MappingConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using foodheaven.services.couponapi.Models.Dto;
+using foodheaven.services.couponapi.Models;
+
+namespace foodheaven.services.couponapi
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps() 
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<CouponDto, Coupon>();
+                config.CreateMap<Coupon, CouponDto>();
+            });
+            return mappingConfig;
+        }
+    }
+}
